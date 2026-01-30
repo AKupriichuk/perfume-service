b6a1591b2bc6cf9f02e6c19a9e17a4d4c8b22534
AKupriichuk <alexa05kup@gmail.com>
2026-01-30 14:36:35 +0200

    c3 work

--- perfume/urls.py
+++ perfume/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+
+from . import views
 from .views import index
 
 urlpatterns = [
-    path("", index), # Порожні лапки означають головну сторінку
+    path("", index),
+    path('perfume/', views.PerfumeListView.as_view(), name='perfumes'),
+    path('manufacturers/', views.ManufacturerListView.as_view(), name='manufacturers'),
+    path('perfume/<int:pk>/', views.PerfumeDetailView.as_view(), name='perfume_detail'),
 ]
--- perfume/views.py
+++ perfume/views.py
@@ -1,4 +1,8 @@
+from django.views import generic
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
 from .models import Perfume, Manufacturer
 
 
@@ -15,4 +19,24 @@ def index(request):
         "num_visits": num_visits + 1,
     }
 
-    return render(request, "perfume/perfume_list.html", context=context)
+    return render(request, "perfume/index.html", context=context)
+
+
+class PerfumeListView(generic.ListView):
+    model = Perfume
+    context_object_name = "perfumes"
+    queryset = Perfume.objects.select_related("manufacturer")
+    template_name = "perfume/perfume_list.html"
+    paginate_by = 3
+
+
+class PerfumeDetailView(generic.DetailView):
+    model = Perfume
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    context_object_name = "manufacturers"
+    queryset = Manufacturer.objects.all()
+    template_name = "perfume/manufacturers_list.html"
+    paginate_by = 3
