6a76d120f506a2129d4db51c95b7c5e3028fc7fe
AKupriichuk <alexa05kup@gmail.com>
2026-02-10 00:39:57 +0200

    fix: eror 500

--- perfume_service/settings/prod.py
+++ perfume_service/settings/prod.py
@@ -1,4 +1,6 @@
+import os
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
@@ -6,10 +8,12 @@ ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    CSRF_TRUSTED_ORIGINS = [f'https://{RENDER_EXTERNAL_HOSTNAME}']
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
